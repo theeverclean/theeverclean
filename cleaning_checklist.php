a8f8cd69c163201112122caa9c8e0196753a493a
theeverclean <cleaningsseattle@gmail.com>
2026-03-23 21:27:13 -0700

    Add files via upload

---
+++ cleaning_checklist.php
@@ -0,0 +1,131 @@
+	<div class="row gray full-width page-header vertical-align-table">
+		<div class="row">
+			<div class="page-header-left">
+				<h1>CLEANING CHECKLIST</h1>
+			</div>
+			<div class="page-header-right">
+				<div class="bread-crumb-container">
+					<ul class="bread-crumb">
+						<li>
+							<a title="Home" href="?page=home">
+								Home
+							</a>
+						</li>
+						<li class="separator">
+							&#47;
+						</li>
+						<li>
+							Cleaning Checklist
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="theme-page">
+	<div class="row full-width header-background header-background-3"></div>
+	<div class="clearfix">
+		<div class="row full-width gray page-padding-top padding-bottom-100">
+			<div class="row">
+				<p class="description align-center">We use a house cleaning checklist while cleaning houses.<br>These have always kept our staff focused on what needs to be done.</p>
+				<ul class="services-list services-icons margin-top-65 clearfix">
+					<li class="column column-1-3">
+						<span class="service-icon big features-toilet tick"></span>
+						<h4>BATHROOM</h4>
+						<ul class="list padding-left-right-50">
+							<li class="template-tick-1">Clean and sanitize toilet &amp; tubs</li>
+							<li class="template-tick-1">Clean and sanitize sinks &amp; showers</li>
+							<li class="template-tick-1">Clean mirrors and glass</li>
+							<li class="template-tick-1">Dust light fixtures and bulbs</li>
+							<li class="template-tick-1">Wash floors</li>
+						</ul>
+					</li>
+					<li class="column column-1-3">
+						<span class="service-icon big features-cooking tick"></span>
+						<h4>KITCHEN</h4>
+						<ul class="list padding-left-right-50">
+							<li class="template-tick-1">Wipe down cabinets</li>
+							<li class="template-tick-1">Clean appliances outside</li>
+							<li class="template-tick-1">Clean refrigerator &amp; oven</li>
+							<li class="template-tick-1">Clean and sanitize sinks</li>
+							<li class="template-tick-1">Sweep and wash floors</li>
+						</ul>
+					</li>
+					<li class="column column-1-3">
+						<span class="service-icon big features-bedroom tick"></span>
+						<h4>BEDROOM</h4>
+						<ul class="list padding-left-right-50">
+							<li class="template-tick-1">Make beds</li>
+							<li class="template-tick-1">Change sheets</li>
+							<li class="template-tick-1">Dust furniture</li>
+							<li class="template-tick-1">Vacuum floor</li>
+							<li class="template-tick-1">Put away clean laundry</li>
+						</ul>
+					</li>
+				</ul>
+				<ul class="services-list services-icons margin-top-30 clearfix">
+					<li class="column column-1-3">
+						<span class="service-icon big features-armchair tick"></span>
+						<h4>LIVING ROOM</h4>
+						<ul class="list padding-left-right-50">
+							<li class="template-tick-1">Dust all hard surfaces and shelves</li>
+							<li class="template-tick-1">Vacuum carpets and rugs</li>
+							<li class="template-tick-1">Sweep mop hard floors</li>
+							<li class="template-tick-1">Vacuum upholstered furniture</li>
+							<li class="template-tick-1">Polish wood</li>
+						</ul>
+					</li>
+					<li class="column column-1-3">
+						<span class="service-icon big features-washing-machine tick"></span>
+						<h4>LAUNDRY</h4>
+						<ul class="list padding-left-right-50">
+							<li class="template-tick-1">Clean washer and dryer exteriors</li>
+							<li class="template-tick-1">Clean lint traps</li>
+							<li class="template-tick-1">Empty wastebasket</li>
+							<li class="template-tick-1">Sweep and mop floor</li>
+							<li class="template-tick-1">Clean loundry sink and fixtures</li>
+						</ul>
+					</li>
+					<li class="column column-1-3">
+						<span class="service-icon big features-watering-can tick"></span>
+						<h4>OUTDOORS</h4>
+						<ul class="list padding-left-right-50">
+							<li class="template-tick-1">Empty wastebaskets</li>
+							<li class="template-tick-1">Mowing the lawn</li>
+							<li class="template-tick-1">Watering plants</li>
+							<li class="template-tick-1">Put away tools and supplies</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row page-margin-top-section padding-bottom-100">
+			<div class="column column-1-2">
+				<h2 class="box-header">ABOUT OUR COMPANY</h2>
+				<p class="description align-center">Exceptional level of cleaning services.</p>
+				<p class="align-center padding-0 margin-top-27 padding-left-right-35">Founded in 1995 Cleanmate quickly built a reputation as one of the leading providers of residential and commercial cleaning solutions. Our continuous pursuit for perfection has resulted in consistent growth each year. Our focus is to listen to our clients, understand their needs and provide the exceptional level of residential and commercial cleaning service.</p>
+				<div class="align-center page-margin-top padding-bottom-16">
+					<a class="more" href="?page=about" title="Learn more">Learn more</a>
+				</div>
+			</div>
+			<div class="column column-1-4">
+				<a href="images/samples/480x693/placeholder.jpg" class="prettyPhoto cm-preload" title="Gutter Cleaning">
+					<img src='images/samples/480x693/placeholder.jpg' alt='img'>
+				</a>
+			</div>
+			<div class="column column-1-4">
+				<div class="row">
+					<a href="images/samples/480x320/placeholder.jpg" class="prettyPhoto cm-preload" title="House Cleaning">
+						<img src='images/samples/480x320/placeholder.jpg' alt='img'>
+					</a>
+				</div>
+				<div class="row margin-top-30">
+					<a href="images/samples/480x320/placeholder.jpg" class="prettyPhoto cm-preload" title="After Renovation Cleaning">
+						<img src='images/samples/480x320/placeholder.jpg' alt='img'>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ contact.php
@@ -0,0 +1,54 @@
+<div class="theme-page padding-bottom-100">
+	<div class="clearfix">
+		<div class="row full-width">
+			<div class="row relative">
+				<div class="contact-map-details">
+					<h4>CLEANMATE LLC</h4>
+					<p class="description">745 Adelaide Street<br>Ottawa ON K1A 5Y7<br>Ontario, Canada</p>
+					<ul class="social-icons gray margin-top-42">
+						<li>
+							<a target="_blank" href="https://twitter.com/QuanticaLabs" class="social-twitter" title="twitter"></a>
+						</li>
+						<li>
+							<a href="https://pinterest.com/quanticalabs/" class="social-pinterest" title="pinterest"></a>
+						</li>
+						<li>
+							<a target="_blank" href="https://facebook.com/QuanticaLabs" class="social-facebook" title="facebook"></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="contact-map" id="map" data-scroll-wheel="0" data-marker-lat="45.4005763" data-marker-lng="-75.6837527" data-map-center-lat="45.4005763" data-map-center-lng="-75.6805812"></div>
+		</div>
+		<div class="row margin-top-89">
+			<h2 class="align-center green">Please call now: <a href="tel:2507257052">250 725 7052</a></h2>
+			<p class="description align-center margin-top-20">or send us a message</p>
+		</div>
+		<div class="row page-margin-top">
+			<form class="contact-form" id="contact-form" method="post" action="contact_form/contact_form.php">
+				<div class="row flex-box">
+					<fieldset class="column column-1-2">
+						<label>YOUR NAME</label>
+						<input class="text-input" name="name" type="text" value="">
+						<label>YOUR EMAIL</label>
+						<input class="text-input" name="email" type="text" value="">
+						<label>YOUR PHONE</label>
+						<input class="text-input" name="phone" type="text" value="">
+					</fieldset>
+					<fieldset class="column column-1-2">
+						<label>MESSAGE</label>
+						<textarea name="message"></textarea>
+					</fieldset>
+				</div>
+				<div class="row margin-top-30">
+					<div class="column column-1-1">
+						<input type="hidden" name="action" value="contact_form" />
+						<div class="row margin-top-15 padding-bottom-16 align-center">
+							<a class="more submit-contact-form" href="#" title="Send message">Send message</a>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
